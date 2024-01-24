1633252279708f5c6fd5b1667e99f3680d05b368
sarahszwarc <135852972+sarahszwarc@users.noreply.github.com>
2024-01-24 14:12:25 +0100

    Add files via upload

---
+++ Modified_cosine_score_heatmaps.py
@@ -0,0 +1,42 @@
+# Libraries and modules import
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matchms import calculate_scores
+from matplotlib import pyplot as plt
+from matchms.importing import load_from_mgf
+from matchms.similarity import ModifiedCosine
+
+#Setting your path
+path_root = "directory_path" #input the directory path
+file = os.path.join(path_root,"filename") #input the filename
+
+#Creating a spectra list
+spectra = list(load_from_mgf(file))
+
+#Calculating the similarity score
+similarity_measure = ModifiedCosine(tolerance=0.005) #you're free to select another similarity score and to set the tolerance (don't forget to import the module before)
+scores = calculate_scores(spectra, spectra, similarity_measure, is_symmetric=True)
+scores_array = scores.scores.to_array()
+
+m = 321 #Modify the value with the number of spectra that you want to plot
+
+scores_array = np.zeros((m,m),dtype=float) #Creating an array of size m by m
+for sp_i in range(len(spectra)):
+    for sp_j in range(sp_i,len(spectra)):
+        sc=float(similarity_measure.pair(spectra[sp_i],spectra[sp_j])['score'])
+        scores_array[sp_i][sp_j] = sc
+        scores_array[sp_j][sp_i] = sc
+
+#Axes definition
+#You're free to input the metadata you want to visualize in your heatmap
+l = [s.metadata['title'] for s in spectra]
+sk = [s.metadata['skeleton'] for s in spectra]
+df = pd.DataFrame(scores_array, columns=l, index=sk)
+
+#Heatmap plotting
+sns.set (rc = {'figure.figsize':(100, 100)}) #You can modulate the output size
+sns.heatmap(df,cmap='viridis') #You can change the viridis value to set another color palette
+plt.title('MIADB Modified Cosine Heatmap') #Set your title here
+plt.show()
